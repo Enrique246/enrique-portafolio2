6b1ed929503c0bf5ebc405a7abfdb88d7a97a80a
Henri <ecch9210@gmail.com>
2021-08-26 12:31:39 -0500

    Updated resume 26 Aug 2021

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div>
       <h1 >Resume</h1>
-<a href="images/Tech Resume 2 copy.pdf" target="blank_"> Click to view complete resume</a>
+<a href="images/Tech Resume 2 (3).pdf" target="blank_"> Click to view complete resume</a>
       <div className="container card ">
         <img className="card-img-top mt-2" src="images/Image Resume.png" alt="Big card"/>
       </div> 
